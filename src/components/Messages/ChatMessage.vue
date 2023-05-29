c605246830c8dd5299cea1112440d41ee55a7337
qcgm197874@gmail.com <qcgm197874@gmail.com>
2023-05-29 11:19:22 +0800

    * style(ChatMessage.vue): fix indentation and spacing in template and style blocks

--- src/components/Messages/ChatMessage.vue
+++ src/components/Messages/ChatMessage.vue
@@ -1,24 +1,14 @@
 <template>
-  <v-card
-    ref="root"
-    :class="[
-      'message',
-      message.type,
-      message.highlight ? 'highlight-border' : '',
-    ]"
-    :loading="message.done ? false : 'primary'"
-  >
+  <v-card ref="root" :class="[
+    'message',
+    message.type,
+    message.highlight ? 'highlight-border' : '',
+  ]" :loading="message.done ? false : 'primary'">
     <v-card-title v-if="message.type === 'response'" class="title">
       <img :src="botLogo" alt="Bot Icon" />
       {{ botFullname }}
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        size="x-small"
-        icon
-        @click="toggleHighlight"
-        :color="message.highlight ? 'primary' : ''"
-      >
+      <v-btn flat size="x-small" icon @click="toggleHighlight" :color="message.highlight ? 'primary' : ''">
         <v-icon>mdi-lightbulb-on-outline</v-icon>
       </v-btn>
       <v-btn flat size="x-small" icon @click="copyToClipboard">
@@ -28,14 +18,8 @@
         <v-icon>mdi-delete</v-icon>
       </v-btn>
     </v-card-title>
-    <Markdown
-      class="markdown-body"
-      :breaks="true"
-      :html="true"
-      :source="message.content"
-      @click="handleClick"
-    />
-    <VueVega />
+    <Markdown class="markdown-body" :breaks="true" :html="true" :source="message.content" @click="handleClick" />
+    <VueVega :source="message.content" />
   </v-card>
   <ConfirmModal ref="confirmModal" />
 </template>
@@ -133,45 +117,45 @@ function handleClick(event) {
 
 <style scoped>
 .message {
-    border-radius: 8px;
-    padding: 16px;
-    word-wrap: break-word;
-    text-align: left;
+  border-radius: 8px;
+  padding: 16px;
+  word-wrap: break-word;
+  text-align: left;
 }
 
 .highlight-border {
-    box-shadow: 0 0 0 2px rgba(var(--v-theme-primary), 1);
+  box-shadow: 0 0 0 2px rgba(var(--v-theme-primary), 1);
 }
 
 .prompt {
-    background-color: #95EC69;
-    width: fit-content;
-    grid-column: 1 / span var(--columns);
+  background-color: #95EC69;
+  width: fit-content;
+  grid-column: 1 / span var(--columns);
 }
 
 .response {
-    background-color: #FFF;
-    width: 100%;
-    grid-column: auto / span 1;
+  background-color: #FFF;
+  width: 100%;
+  grid-column: auto / span 1;
 }
 
 .title {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 1rem;
-    padding: 0;
-    margin-bottom: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  padding: 0;
+  margin-bottom: 8px;
 }
 
 .title img {
-    width: 20px;
-    height: 20px;
-    margin-right: 4px;
+  width: 20px;
+  height: 20px;
+  margin-right: 4px;
 }
 
 .markdown-body {
-    background-color: inherit;
-    font-family: inherit;
+  background-color: inherit;
+  font-family: inherit;
 }
 </style>
