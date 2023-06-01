9f83ec625d077778aaaefd1581e792e905111034
qcgm197874@gmail.com <qcgm197874@gmail.com>
2023-06-01 11:17:00 +0800

    * refactor(App.vue): filter bots to check by selectedBots.value * refactor(main.js): remove unused Matomo configuration options

--- src/main.js
+++ src/main.js
@@ -93,41 +93,12 @@ createApp(App)
     // Require consent before creating matomo session cookie
     // Default: false
     requireCookieConsent: false,
-
-    // Enable the heartbeat timer (https://developer.matomo.org/guides/tracking-javascript-guide#accurately-measure-the-time-spent-on-each-page)
-    // Default: false
     enableHeartBeatTimer: false,
-
-    // Set the heartbeat timer interval
-    // Default: 15
     heartBeatTimerInterval: 15,
-
-    // Whether or not to log debug information
-    // Default: false
     debug: false,
-
-    // UserID passed to Matomo (see https://developer.matomo.org/guides/tracking-javascript-guide#user-id)
-    // Default: undefined
     userId: undefined,
-
-    // Share the tracking cookie across subdomains (see https://developer.matomo.org/guides/tracking-javascript-guide#measuring-domains-andor-sub-domains)
-    // Default: undefined, example '*.example.com'
     cookieDomain: undefined,
-    // setDomains: [".", "localhost", "127.0.0.1"],
-    // enableCrossDomainLinking: true,
-
-    // Tell Matomo the website domain so that clicks on these domains are not tracked as 'Outlinks'
-    // Default: undefined, example: '*.example.com'
     domains: "*",
-
-    // A list of pre-initialization actions that run before matomo is loaded
-    // Default: []
-    // Example: [
-    //   ['API_method_name', parameter_list],
-    //   ['setCustomVariable','1','VisitorType','Member'],
-    //   ['appendToTrackingUrl', 'new_visit=1'],
-    //   etc.
-    // ]
     preInitActions: [
       [
         "setCustomVariable",
@@ -137,25 +108,7 @@ createApp(App)
         "visit",
       ],
     ],
-
-    // A function to determine whether to track an interaction as a site search
-    // instead of as a page view. If not a function, all interactions will be
-    // tracked as page views. Receives the new route as an argument, and
-    // returns either an object of keyword, category (optional) and resultsCount
-    // (optional) to track as a site search, or a falsey value to track as a page
-    // view.
-    // Default: false, i.e. track all interactions as page views
-    // Example: (to) => {
-    //   if (to.query.q && to.name === 'search') {
-    //     return { keyword: to.query.q, category: to.params.category }
-    //   } else {
-    //    return null
-    //   }
-    // }
     trackSiteSearch: false,
-
-    // Set this to include crossorigin attribute on the matomo script import
-    // Default: undefined, possible values : 'anonymous', 'use-credentials'
     crossOrigin: undefined,
   })
   .mount("#app");
